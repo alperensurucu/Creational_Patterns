22be5a137c01aa4e984e79442c5963bb3353b19f
alperen surucu <alperensurucu.eng@gmail.com>
2026-03-12 23:36:32 +0300

    singleton design patter easy example_another way

---
+++ Singleton_Design_Pattern/Exam.cs
@@ -0,0 +1,25 @@
+﻿
+// ikinci yöntemde static ctorda üretiyoruz nesneyi.
+class Exam
+{
+    static void Main(string[] args)
+    {
+        Exam exeee = Exam.GetInstace;
+    }
+
+    private Exam()
+    {
+         Console.WriteLine($"{nameof(Exam)}");   
+    }
+
+    static Exam()
+    {
+        _exam = new Exam();
+    }
+    static Exam _exam;
+
+    public static Exam GetInstace
+    {
+        get { return _exam; }
+    }
+}
--- Singleton_Design_Pattern/Example.cs
+++ Singleton_Design_Pattern/Example.cs
@@ -18,6 +18,10 @@ Example nesnesi oluşturuldu
 //new Example();
 
 Example ex = Example.GetInstance;
+Example ex1 = Example.GetInstance;
+Example ex2= Example.GetInstance;
+Example ex3 = Example.GetInstance;
+Example ex4 = Example.GetInstance;    // yine bir tane nesne oluşacak ve output => Example nesnesi oluşturuldu.
 class Example
 {
     private Example()
@@ -31,7 +35,7 @@ class Example
     {
         get
         {
-            if (_example == null)
+            if (_example == null)         // burada bir  kontrol var ise bu 1. YÖNTEM. <<<<<================
             {
                 _example = new Example();
             }
