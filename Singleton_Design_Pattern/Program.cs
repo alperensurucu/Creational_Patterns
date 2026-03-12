706932cf707ba9eaaa79043b5c5192f1b43580a8
alperen surucu <alperensurucu.eng@gmail.com>
2026-03-12 20:48:57 +0300

    Initial commit

---
+++ Singleton_Design_Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton_Design_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Example();
+            new Example();
+            new Example();
+            new Example();
+            new Example();
+            new Example();
+            new Example();
+            new Example();
+            new Example();
+        }
+    }
+
+    class Example
+    {
+        public Example()
+        {
+                Console.WriteLine($"{nameof(Example)} nesnesi oluşturuldu");
+        }
+    }
+}
