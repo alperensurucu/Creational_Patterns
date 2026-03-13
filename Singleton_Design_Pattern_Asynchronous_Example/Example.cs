9b8dd83154e2afc87f7d10be983a7a7cbf962e00
alperen surucu <alperensurucu.eng@gmail.com>
2026-03-13 19:03:34 +0300

    bildiğimiz singleton design pattern buraya kadar , bir classın singleton ile tek bir nesne üretmeye zorlama ve başka bir nesne üretilmemesi üzerine garanti edilmesi

---
+++ Singleton_Design_Pattern_Asynchronous_Example/Example.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern_Asynchronous_Example
+{
+    class Example
+    {
+        private Example()
+        {
+            Console.WriteLine($"{nameof(Example)} object created.");     
+        }
+        static Example _example;
+        public static Example GetInstane
+        {
+            get
+            {
+                if (_example == null)
+                {
+                    _example = new Example();
+                }
+                return _example;
+            }
+        }
+    }
+}
---
+++ Singleton_Design_Pattern_Asynchronous_Example/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Singleton_Design_Pattern_Asynchronous_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example example = Example.GetInstane;
+            Example example1 = Example.GetInstane;
+            Example example2 = Example.GetInstane;
+            Example example3 = Example.GetInstane;
+        }
+    }
+
+}
