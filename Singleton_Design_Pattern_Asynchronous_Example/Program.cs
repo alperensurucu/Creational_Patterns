d1c3485c8713c82a5d889cb297a0fb51c1076b7e
alperen surucu <alperensurucu.eng@gmail.com>
2026-03-13 19:26:49 +0300

    thirt  sdp yöntemi static ctor içinden

--- Singleton_Design_Pattern_Asynchronous_Example/Program.cs
+++ Singleton_Design_Pattern_Asynchronous_Example/Program.cs
@@ -4,11 +4,17 @@
     {
         static void Main(string[] args)
         {
-            var t1 = Task.Run(() => { Example example = Example.GetInstane; });
-            var t2 = Task.Run(() => { Example example = Example.GetInstane; });
-            var t3 = Task.Run(() => { Example example = Example.GetInstane; });
-            var t4 = Task.Run(() => { Example example = Example.GetInstane; });
-            var t5 = Task.Run(() => { Example example = Example.GetInstane; });
+            //var t1 = Task.Run(() => { Example example = Example.GetInstane; });
+            //var t2 = Task.Run(() => { Example example = Example.GetInstane; });
+            //var t3 = Task.Run(() => { Example example = Example.GetInstane; });
+            //var t4 = Task.Run(() => { Example example = Example.GetInstane; });
+            //var t5 = Task.Run(() => { Example example = Example.GetInstane; });
+
+            var t1 = Task.Run(() => { Example example = Example.GetInstane(); });
+            var t2 = Task.Run(() => { Example example = Example.GetInstane(); });
+            var t3 = Task.Run(() => { Example example = Example.GetInstane(); });
+            var t4 = Task.Run(() => { Example example = Example.GetInstane(); });
+            var t5 = Task.Run(() => { Example example = Example.GetInstane(); });
         }
     }
 
