888801eb5c91f02ca5d74288379220e87fcfb0c8
alperen surucu <alperensurucu.eng@gmail.com>
2026-03-14 16:10:23 +0300

    multition_design_Patter

---
+++ Multition_Design_Pattern_Example/Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multition_Design_Pattern_Example
+{
+    class Database
+    {
+        private Database()
+        {
+            Console.WriteLine($"{nameof(Database)} object is created. ");
+        }
+
+        //static Database _database; // bunu artık kullanmıyoruz tekil tutmayacağız diye .
+
+        static Dictionary<string, Database> _databases = new();
+
+        public static Database GetInstance(string key)  // sınırlı sayıda key kadar nesne oluşturuyoruz .
+        {
+            if (_databases.ContainsKey(key))  //database nesnesi yoksa   _databases[key] e karşı bir tane database nesnesi üret diyoruz.
+            {
+                _databases[key] = new Database();
+            }
+            return _databases[key];
+        }
+
+        public void Connection()
+        {
+            Console.WriteLine("Connected.");
+        }
+        public void Disconnection()
+        {
+            Console.WriteLine("Disconnected.");
+        }
+
+        public void ConnectionString(string connectionString)
+        {
+            //...
+        }
+    }
+}
---
+++ Multition_Design_Pattern_Example/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Multition_Design_Pattern_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mssql = Database.GetInstance("MSSQL");
+            mssql.ConnectionString("sdfsfdsgfsdasdasdasdasdasdsadas");
+            mssql.Connection();
+            var oracle = Database.GetInstance("Oracle");
+            var mongoDB = Database.GetInstance("MongoDB");
+        }
+    }
+}
